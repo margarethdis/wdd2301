7bcfd88e3e07aeebf4283dc2d0e1f7fd8caa5990
margarethdis <margarethdani2021@gmail.com>
2024-03-23 20:41:37 -0600

    .

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -1,14 +1,8 @@
-
-
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#menu').addEventListener('click', function() {
         document.querySelector('.nav').classList.toggle('show');
-        document.querySelector('#menu').classList.toggle('open');
-    });
-
-    // Cerrar el menú al hacer clic en el botón "X"
+        document.querySelector('#menu').classList.toggle('open');});
     document.querySelector('.close-menu').addEventListener('click', function() {
         document.querySelector('.nav').classList.remove('show');
         document.querySelector('#menu').classList.remove('open');
-    });
-});
+    });});
